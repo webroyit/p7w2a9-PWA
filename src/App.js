635cdba48caf7000d651256518ef119b883cd4b1
royweb <xytank@gmail.com>
2020-07-29 13:05:42 -0400

    Add input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { fetchWeather } from './api/fetch';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+
   return (
-    <>
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)} />
       <h1>Hello</h1>
-    </>
+    </div>
   );
 }
 
