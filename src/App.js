141ca15f4e98c7b10da394ffcd511f32a14cad43
royweb <xytank@gmail.com>
2020-07-29 13:40:05 -0400

    Add a function for search bar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import './App.css';
 function App() {
   const [query, setQuery] = useState('');
 
+  const search = async (e) => {
+    if(e.key === 'Enter'){
+      const data = await fetchWeather(query);
+
+      console.log(data);
+      setQuery('');
+    }
+  }
+
   return (
     <div>
       <input
         type="text"
         placeholder="Search..."
         value={query}
-        onChange={(e) => setQuery(e.target.value)} />
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search} />
       <h1>Hello</h1>
     </div>
   );
