817b0d082ae499ca3cbfe9bb30115ad607fcebed
royweb <xytank@gmail.com>
2020-07-29 14:19:25 -0400

    Display weather data

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 
 function App() {
   const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
 
   const search = async (e) => {
     if(e.key === 'Enter'){
       const data = await fetchWeather(query);
 
-      console.log(data);
+      setWeather(data);
       setQuery('');
     }
   }
@@ -23,7 +24,23 @@ function App() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search} />
-      <h1>Hello</h1>
+      
+      {weather.main && (
+        <div>
+          <h2>
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div>
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
